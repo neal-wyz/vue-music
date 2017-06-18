f2d486a3ce82c08ceaf367a713b241a34b1af478
jack <jack.wang@shun-lu.cn>
2017-06-19 00:06:41 +0800

    1.推荐页面 2.slider组件 3.scroll组件 4.loading组件 5.jsonp获取qq音乐数据 6.vue-lazyload懒加载

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 import 'common/scss/index.scss'
+import VueLazyLoad from 'vue-lazyload'
 
 fastclick.attach(document.body)
 
-Vue.config.productionTip = false
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
